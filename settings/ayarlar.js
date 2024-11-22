b715b50186bd768f42380819b2d7b2c73c0739d4
faridasad <asadlifaridd@gmail.com>
2024-11-22 18:12:46 +0400

    init

---
+++ settings/ayarlar.js
@@ -0,0 +1,7 @@
+require("dotenv").config();
+
+module.exports = {
+    TOKEN: process.env.TOKEN || "",  // lrows token
+    PREFIX: process.env.PREFIX || "!", // lrows prefix
+    SAHİP: process.env.SAHİP || " ", //lrows id
+}
